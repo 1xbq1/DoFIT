b777514d2a6d0339b42423dfaf49aebe2bf5fce4
1xbq1 <105406464+1xbq1@users.noreply.github.com>
2024-10-06 22:51:23 +0800

    Delete evaluation/open_ended directory

--- evaluation/open_ended/test.sh
+++
@@ -1,2 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python gen_model_answer_mt.py --base_model_path "../../output/alpaca-gpt4/full-200" --template "alpaca"
-CUDA_VISIBLE_DEVICES=1 python gen_model_answer.py --base_model_path "../../output/alpaca-gpt4/full-200" --template "alpaca" --bench_name "vicuna"
